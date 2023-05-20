5a218f52e39225b68bd2030aa1222b6eed049f6f
Kadu <kadu.silva2014@gmail.com>
2023-05-20 18:41:04 -0300

    ✨ feat(server): config token jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { authRoutes, memoriesRotes } from './routes'
 
 import 'dotenv/config'
@@ -10,6 +11,9 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+app.register(jwt, {
+  secret: process.env.JWT_SECRET || 'secret',
 })
 
 app.register(memoriesRotes)
